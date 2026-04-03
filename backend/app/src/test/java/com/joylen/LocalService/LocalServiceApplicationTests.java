259e3e96c31e4a8c618c126cc3ef0081e6c0edbf
JOYLEN-DIAS <diasjoylen06@gmail.com>
2026-04-03 12:51:26 +0530

    Initial project structure (frontend + backend)

---
+++ backend/app/src/test/java/com/joylen/LocalService/LocalServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.joylen.LocalService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LocalServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
